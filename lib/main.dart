dc90a4705903a3eaf59471077f02ae977146a4a4
AMAbdulrahim <98281994+AMAbdulrahim@users.noreply.github.com>
2025-02-12 21:13:04 +0300

    routes file added and main file updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:laser_car_battle/assets/theme/custom_theme.dart';
-import 'package:laser_car_battle/views/landing_page.dart';
+import 'package:laser_car_battle/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: CustomTheme.darkTheme,
-      home: LandingPage(),
+      initialRoute: '/home',
+      routes: routes,
     
     );
   }
