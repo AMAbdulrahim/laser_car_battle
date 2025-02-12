e8c5e794ab9a32149543ba76ff77b4936cb42ef3
AMAbdulrahim <98281994+AMAbdulrahim@users.noreply.github.com>
2025-02-12 14:32:46 +0300

    text effects on the project_title

--- lib/widgets/project_title.dart
+++ lib/widgets/project_title.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:laser_car_battle/assets/theme/colors/color.dart';
 import 'package:laser_car_battle/utils/constants.dart';
 
 class TitleText extends StatelessWidget {
@@ -6,20 +7,93 @@ class TitleText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(
-          'Team',
-          style: TextStyle(
-            fontSize: AppSizes.fontTitle,
-          ),
+        Stack(
+          children: <Widget>[
+            // Stroked text as border.
+            Text(
+              'Team',
+              style: TextStyle(
+                fontSize: AppSizes.fontTitle,
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 6
+                  ..color = CustomColors.effectColor,
+                shadows: [
+                  Shadow(
+                    offset: Offset(-1.5, -1.5),
+                    color: CustomColors.effectColor,
+                  ),
+                  Shadow(
+                    offset: Offset(1.5, 1.5),
+                    color: CustomColors.effectColor,
+                  ),
+                ],
+              ),
+            ),
+            // Solid text as fill.
+            Text(
+              'Team',
+              style: TextStyle(
+                fontSize: AppSizes.fontTitle,
+                shadows: [
+                  Shadow(
+                    offset: Offset(-1.5, -1.5),
+                    color: CustomColors.effectColor,
+                  ),
+                  Shadow(
+                    offset: Offset(1.5, 1.5),
+                    color: CustomColors.effectColor,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        Text(
-          '57',
-          style: TextStyle(
-            fontSize: AppSizes.fontTitle,
-          ),
+        Stack(
+          children: <Widget>[
+            // Stroked text as border.
+            Text(
+              '57',
+              style: TextStyle(
+                fontSize: AppSizes.fontTitle,
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 6
+                  ..color = CustomColors.effectColor,
+                shadows: [
+                  Shadow(
+                    offset: Offset(-1.5, -1.5),
+                    color: CustomColors.effectColor,
+                  ),
+                  Shadow(
+                    offset: Offset(1.5, 1.5),
+                    color: CustomColors.effectColor,
+                  ),
+                ],
+              ),
+            ),
+            // Solid text as fill.
+            Text(
+              '57',
+              style: TextStyle(
+                fontSize: AppSizes.fontTitle,
+                shadows: [
+                  Shadow(
+                    offset: Offset(-1.5, -1.5),
+                    color: CustomColors.effectColor,
+                  ),
+                  Shadow(
+                    offset: Offset(1.5, 1.5),
+                    color: CustomColors.effectColor,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ],
     );
