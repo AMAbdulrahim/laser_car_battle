0454374701985d0ee97bc1a4c3c1519ae49e653d
AMAbdulrahim <98281994+AMAbdulrahim@users.noreply.github.com>
2025-02-17 21:27:00 +0300

    controller page skeleton

--- lib/views/controller_page.dart
+++ lib/views/controller_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:laser_car_battle/utils/constants.dart';
 import 'package:laser_car_battle/widgets/custom/custom_app_bar.dart';
 
 class RemoteController extends StatefulWidget {
@@ -31,10 +32,155 @@ class _RemoteControllerState extends State<RemoteController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      body: const Center(
-        child: Text('Remote Controller Page'),
-        
+      body: Stack(
+        children: [
+          // Settings Indicator
+          Positioned(
+            top: AppSizes.paddingLarge,
+            left: AppSizes.paddingMedium,
+            child: Container(
+              width: 75,
+              height: 75,
+              decoration: BoxDecoration(
+                color: Colors.green.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const Center(child: Text('settings')),
+            ),
+          ),
+          // insights widget
+          Positioned(
+            top: AppSizes.paddingLarge,
+            right: AppSizes.paddingMedium,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.green.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const Center(child: Text('insights')),
+            ),
+          ),
+          // Top Center Widget
+          Positioned(
+            top: AppSizes.paddingLarge,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: 600,
+                height: 120,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // Player 1 name section section
+                    Container(
+                      width: 200,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.blue.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(child: Text('Player 1')),
+                    ),
+                    // Middle section
+                    Container(
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.purple.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          // points section
+                          Container(
+                            height: 70,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Colors.purple.withOpacity(0.2),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: const Center(child: Text('Points')),
+                          ),
+                          // Timer section
+                          Container(
+                            height: 50,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Colors.purple.withOpacity(0.2),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: const Center(child: Text('Timer')),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Player 2 name section
+                    Container(
+                      width: 200,
+                      height: 60,
+
+                      decoration: BoxDecoration(
+                        color: Colors.blue.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(child: Text('Player 2')),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          // Bottom Left Widget
+          Positioned(
+            bottom: AppSizes.paddingLarge + 10,
+            left: AppSizes.paddingLarge + 10,
+            child: Row(
+              children: [
+                // Brake control widget
+                Container(
+                  width: 100,
+                  height: 150,
+                  margin: const EdgeInsets.only(right: AppSizes.paddingMedium),
+                  decoration: BoxDecoration(
+                    color: Colors.green.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Center(child: Text('Brake')),
+                ),
+                // Fire button control widget
+                Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.green.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Center(child: Text('Fire button')),
+                ),
+              ],
+            ),
+          ),
+
+          // Joystick Widget
+          Positioned(
+            bottom: AppSizes.paddingLarge +10,
+            right: AppSizes.paddingLarge + 20,
+            child: Container(
+              width: 175,
+              height: 175,
+              decoration: BoxDecoration(
+                color: Colors.red.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(child: Text('Joystick')),
+            ),
+          ),
+        ],
       ),
     );
   }
